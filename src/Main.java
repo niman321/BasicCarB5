c6bbc2ea136087711a2211ed15ecdc49fc27d472
nima321 <niman8900@gmail.com>
2023-03-31 23:53:36 +0100

    ready

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       BasicStructure audi=new BasicStructure();
+       audi.color="red";
+       audi.model="a3";
+       audi.power="5555cc";
+    }
+}
